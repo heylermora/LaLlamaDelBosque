22964cb69db3cc9e3e9cf71f9e5188a305e42f74
Johel Mora <johelmora@gmail.com>
2023-02-28 13:18:16 -0600

    Delete Authentication

--- Program.cs
+++ Program.cs
@@ -5,14 +5,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
-   .AddNegotiate();
-
-builder.Services.AddAuthorization(options =>
-{
-	// By default, all incoming requests will be authorized according to the default policy.
-	options.FallbackPolicy = options.DefaultPolicy;
-});
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
@@ -29,10 +21,6 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
 
 app.MapControllerRoute(
 	name: "default",
