c9347dc5ddd5efa38750bc492171834591e9cba5
Johel Mora <progestdevops@hotmail.com>
2023-07-19 21:24:35 -0600

    Add enter to tab

---
+++ wwwroot/js/entertotab.js
@@ -0,0 +1,22 @@
+﻿function enterToTab(event) {
+    if (event.keyCode === 13) {
+        var form = event.target.form;
+        var index = Array.prototype.indexOf.call(form, event.target);
+        var nextElement = form.elements[index + 1];
+
+        if (nextElement) {
+            nextElement.focus();
+            event.preventDefault();
+        }
+    }
+}
+
+function addEnterToTabEvent() {
+    var inputElements = document.querySelectorAll('input, select, textarea');
+
+    for (var i = 0; i < inputElements.length; i++) {
+        inputElements[i].addEventListener('keydown', enterToTab);
+    }
+}
+
+window.addEventListener('DOMContentLoaded', addEnterToTabEvent);
