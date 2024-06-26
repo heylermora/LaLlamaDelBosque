bb87a44f9c63c417011af4becd138e7dfeb6651b
MoiCalderon <136037861+MoiCalderon@users.noreply.github.com>
2024-06-26 17:00:01 -0600

    ISSUE #2 Cabys list added to create products.

---
+++ Controllers/CabysController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace LaLlamaDelBosque.Controllers
+{
+    public class CabysController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetCabysProducts()
+        {
+            var cabysJsonPath = Path.Combine(Directory.GetCurrentDirectory(), "Data", "ListaCabys.json");
+            var cabysJson = System.IO.File.ReadAllText(cabysJsonPath);
+            return Content(cabysJson, "application/json");
+        }
+    }
+}
